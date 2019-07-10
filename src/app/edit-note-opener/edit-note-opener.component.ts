fbc60d13851587dc10616ac01d65123da99f5d6d
Singh <437038@cognizant.com>
2019-07-10 13:41:40 +0530

    initial commit

--- src/app/edit-note-opener/edit-note-opener.component.ts
+++ src/app/edit-note-opener/edit-note-opener.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EditNoteViewComponent } from '../edit-note-view/edit-note-view.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-note-opener',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class EditNoteOpenerComponent {
 
+  constructor(private dialog: MatDialog,
+    private route: ActivatedRoute) {
+    const noteId = +this.route.snapshot.paramMap.get('noteId');
+    const editDialog = this.dialog.open(EditNoteViewComponent, {
+      data: {
+        note: noteId,
+      }
+    });
+   }
 }
--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterService } from '../services/router.service';
+import { Note } from '../note';
 
 @Component({
   selector: 'app-note',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent {
+  @Input()
+  note: Note;
+  constructor(private routerService: RouterService) { }
 
+  openEditView() {
+    this.routerService.routeToEditNoteView(this.note.id);
+  }
 }
